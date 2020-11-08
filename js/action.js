58cc56582e12ddb0c9fd4337112b0c92315de373
unknown <sameepsawant10@gmail.com>
2020-11-08 13:51:38 +0530

    first commit

---
+++ js/action.js
@@ -0,0 +1,36 @@
+ $(document).ready(function(){
+$('.slicefirst').slick({
+  dots: true,
+    autoplay: false,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+});
+     
+ 
+     
+ });
